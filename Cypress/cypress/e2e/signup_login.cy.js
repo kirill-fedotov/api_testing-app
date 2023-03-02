44813b2a24490c8eba6e1f8ff33cd66860f09df6
DESKTOP-1VFRKBR\XXX <fedotov.k8383@gmail.com>
2023-03-02 20:43:07 +0200

    Added test: - Test valid login

--- Cypress/cypress/e2e/signup_login.cy.js
+++ Cypress/cypress/e2e/signup_login.cy.js
@@ -5,14 +5,16 @@
 describe('SignUp', () => {
 
 
-  let randomString = Math.random().toString(30).substring(2)
+    let randomString = Math.random().toString(30).substring(2)
+    let userName = "User" + randomString
+    let email = "User" + randomString + "@mail.com"
 
   it('Test valid signUp', () => {
     cy.intercept("POST", "https://api.realworld.io/api/users").as("newUser")
     cy.visit("http://localhost:4200/")
     cy.get(".nav").contains("Sign up").click();
-    cy.get("[placeholder='Username']").type("User" + randomString)
-    cy.get("[placeholder='Email']").type("User" + randomString + "@mail.com")
+    cy.get("[placeholder='Username']").type(userName)
+    cy.get("[placeholder='Email']").type(email)
     cy.get("[placeholder='Password']").type(randomString)
     cy.get("button").contains("Sign up").click();
 
@@ -24,6 +26,18 @@ describe('SignUp', () => {
     })
     
   })
+
+
+    it('Test valid login', () => {
+        
+        cy.visit("http://localhost:4200/")
+        cy.get(".nav").contains("Sign in").click()
+        cy.get("[placeholder='Email']").type(email)
+        cy.get("[placeholder='Password']").type(randomString)
+        cy.get("button").contains("Sign in").click()
+        cy.get(':nth-child(4) > .nav-link').should('be.visible')
+
+    })
 
 
 
